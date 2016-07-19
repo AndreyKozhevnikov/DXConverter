26fa78425d0841523311389d5ffab8f7d3cfa0cc
kozhevnikov.andrey <kozhevnikov.andrey@KOZHEVNIKOV-W10>
2016-07-19 13:02:35 +0300

    Start

--- DXConverter/Program.cs
+++ DXConverter/Program.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +10,62 @@ using System.Threading.Tasks;
 namespace DXConverter {
     class Program {
         static void Main(string[] args) {
+            AssemblyConverter a = new AssemblyConverter();
+            var l = a.GetVersions();
+            a.ProcessProject(@"c:\!Tickets\!Test\T348599\T348599\", "15.2.5");
+
+        }
+    }
+
+
+    public class AssemblyConverter {
+        public const string defaultPath = @"\\CORP\builds\release\DXDlls\";
+
+        public List<string> GetVersions() {
+            List<string> directories = new List<string>();
+            try {
+                var allDirectories = Directory.GetDirectories(defaultPath);
+                foreach (string directory in allDirectories)
+                    directories.Add(Path.GetFileName(directory));
+            }
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+                Console.Read();
+            }
+            directories.Sort(new VersionComparer());
+            return directories;
+        }
+
+        internal void ProcessProject(string projectFolder, string version) {
+            var listVersions = GetVersions();
+            if (!listVersions.Contains(version))
+                return;
+            var converterPath = GetProjectConverterPath(defaultPath, version);
+            if (converterPath == null)
+                return;
+
+        }
+
+        private string GetProjectConverterPath(string sourcePath, string targetVersion) {
+            string projectConverterConsolePath = Path.Combine(sourcePath, targetVersion, "ProjectConverter-console.exe");
+            //  string projectConverterPath = Path.Combine(sourcePath, targetVersion, "ProjectConverter.exe");
+            if (File.Exists(projectConverterConsolePath))
+                return projectConverterConsolePath;
+            return null;
+        }
+    }
+
+    public class VersionComparer : IComparer<string> {
+
+        public int Compare(string x, string y) {
+            int counter = 0, res = 0;
+            while (counter < 3 && res == 0) {
+                int versionX = Convert.ToInt32(x.Split('.')[counter]);
+                int versionY = Convert.ToInt32(y.Split('.')[counter]);
+                res = Comparer.Default.Compare(versionX, versionY);
+                counter++;
+            }
+            return -res;
         }
     }
 }
