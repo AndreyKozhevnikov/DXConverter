aa2d002622d3469b155b876a33dee83fe2d8716b
kozhevnikov.andrey <andrey.kozhevnikov@devexpress.com>
2020-02-03 10:06:30 +0300

    parameter parser

--- DXConverter/Classes/ParametersParser.cs
+++ DXConverter/Classes/ParametersParser.cs
@@ -5,14 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace DXConverter {
-   public class ParametersParser {
+    public class ParametersParser {
         public ParametersParser(string[] args) {
-
+            var cnt = args.Count();
+            if(cnt >= 3 && cnt <= 5) {
+                ProjectPath = args[0];
+                Version = args[1];
+                WaitForExit = bool.Parse(args[2]);
+                if(cnt == 4) {
+                    bool tmpBool = false;
+                    var isBool = bool.TryParse(args[3], out tmpBool);
+                    if(isBool) {
+                        IsLocalCacheUsed = tmpBool;
+                    } else {
+                        InstalledVersionPath = args[3];
+                    }
+                }
+                if(cnt == 5) {
+                    InstalledVersionPath = args[3];
+                    IsLocalCacheUsed = bool.Parse(args[4]);
+                }
+                IsArgumentsCorrect = true;
+            }
         }
-        public string ProjectPath{ get; set; }
-        public string Version{ get; set; }
-        public bool WaitForExit{ get; set; }
-        public string InstalledVersionPath{ get; set; }
-        public string IsLocalCacheUsed{ get; set; }
+        public string ProjectPath { get; set; }
+        public string Version { get; set; }
+        public bool WaitForExit { get; set; }
+        public string InstalledVersionPath { get; set; }
+        public bool IsLocalCacheUsed { get; set; }
+
+        public bool IsArgumentsCorrect { get; set; }
     }
 }
--- DXConverter/Tests.cs
+++ DXConverter/Tests.cs
@@ -677,8 +677,9 @@ namespace DXConverter {
             Assert.AreEqual(@"c:\!Tickets\T123123 test sbuject\dx123123", parser.ProjectPath);
             Assert.AreEqual("19.2.4", parser.Version);
             Assert.AreEqual(true, parser.WaitForExit);
-            Assert.AreEqual(string.Empty, parser.InstalledVersionPath);
+            Assert.AreEqual(null, parser.InstalledVersionPath);
             Assert.AreEqual(false, parser.IsLocalCacheUsed);
+            Assert.AreEqual(true, parser.IsArgumentsCorrect);
         }
 
         [Test]
@@ -698,6 +699,7 @@ namespace DXConverter {
             Assert.AreEqual(true, parser.WaitForExit);
             Assert.AreEqual(@"c:\Program Files (x86)\DevExpress 19.1\Components\Tools\Components\ProjectConverter.exe", parser.InstalledVersionPath);
             Assert.AreEqual(true, parser.IsLocalCacheUsed);
+            Assert.AreEqual(true, parser.IsArgumentsCorrect);
         }
         [Test]
         public void ParametersParser_4_onlyPath() {
@@ -715,9 +717,10 @@ namespace DXConverter {
             Assert.AreEqual(true, parser.WaitForExit);
             Assert.AreEqual(@"c:\Program Files (x86)\DevExpress 19.1\Components\Tools\Components\ProjectConverter.exe", parser.InstalledVersionPath);
             Assert.AreEqual(false, parser.IsLocalCacheUsed);
+            Assert.AreEqual(true, parser.IsArgumentsCorrect);
         }
         [Test]
-        public void ParametersParser_4_onlyCache() {
+        public void ParametersParser_4_onlyCache_true() {
             //arrange
             var args = new string[4];
             args[0] = @"c:\!Tickets\T123123 test sbuject\dx123123";
@@ -730,8 +733,38 @@ namespace DXConverter {
             Assert.AreEqual(@"c:\!Tickets\T123123 test sbuject\dx123123", parser.ProjectPath);
             Assert.AreEqual("19.2.4", parser.Version);
             Assert.AreEqual(true, parser.WaitForExit);
-            Assert.AreEqual(string.Empty, parser.InstalledVersionPath);
+            Assert.AreEqual(null, parser.InstalledVersionPath);
             Assert.AreEqual(true, parser.IsLocalCacheUsed);
+            Assert.AreEqual(true, parser.IsArgumentsCorrect);
+        }
+        [Test]
+        public void ParametersParser_4_onlyCache_false() {
+            //arrange
+            var args = new string[4];
+            args[0] = @"c:\!Tickets\T123123 test sbuject\dx123123";
+            args[1] = "19.2.4";
+            args[2] = "True";
+            args[3] = "False";
+            //act
+            var parser = new ParametersParser(args);
+            //assert
+            Assert.AreEqual(@"c:\!Tickets\T123123 test sbuject\dx123123", parser.ProjectPath);
+            Assert.AreEqual("19.2.4", parser.Version);
+            Assert.AreEqual(true, parser.WaitForExit);
+            Assert.AreEqual(null, parser.InstalledVersionPath);
+            Assert.AreEqual(false, parser.IsLocalCacheUsed);
+            Assert.AreEqual(true, parser.IsArgumentsCorrect);
+        }
+
+        [Test]
+        public void ParametersParser_1() {
+            //arrange
+            var args = new string[1];
+            args[0] = @"c:\!Tickets\T123123 test sbuject\dx123123";
+            //act
+            var parser = new ParametersParser(args);
+            //assert
+            Assert.AreEqual(false, parser.IsArgumentsCorrect);
         }
 
 
