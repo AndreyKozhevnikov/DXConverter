a1bd14d4becf44b483070620b05e30eaba73c3fc
kozhevnikov.andrey <andrey.kozhevnikov@devexpress.com>
2020-02-04 09:22:20 +0300

    implement SimpleFilePathProvider

---
+++ DXConverter/Classes/FilePathProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DXConverter {
+    interface IFilePathProvider {
+        string GetFilePath(string fileName);
+    }
+
+    public class SimpleFilePathProvider : IFilePathProvider {
+        public string SourceFolderPath { get;set; }
+        public string Version { get;set; }
+
+        public string GetFilePath(string fileName) {
+            return Path.Combine(SourceFolderPath, Version, fileName);
+        }
+    }
+}
--- DXConverter/Tests/IFilePathProviderTests.cs
+++ DXConverter/Tests/IFilePathProviderTests.cs
@@ -3,6 +3,16 @@
 namespace DXConverter {
     [TestFixture]
     public class IFilePathProviderTests {
-
+        [Test]
+        public void SimpleFilePathProvider() {
+            //arrange
+            var prov = new SimpleFilePathProvider();
+            prov.SourceFolderPath = @"\\CORP\builds\release\DXDlls\";
+            prov.Version = "19.2.6";
+            //act
+            var res = prov.GetFilePath("test.dll");
+            //assert
+            Assert.AreEqual(@"\\CORP\builds\release\DXDlls\19.2.6\test.dll", res);
+        }
     }
 }
